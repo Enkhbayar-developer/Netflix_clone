c068400e5dee92686a43cb6efe2eddc1dd887a80
Enkhbayar <enkhe.tech@gmail.com>
2025-04-21 18:39:54 +0800

    postcss-mixins package added

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,7 @@ module.exports = {
               postcssOptions: {
                 plugins: [
                   require("postcss-import"),
+                  require("postcss-mixins"),
                   require("postcss-simple-vars"),
                   require("postcss-nested"),
                   require("autoprefixer"),
